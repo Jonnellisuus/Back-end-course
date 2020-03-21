d4654cfe26b0f42650bcf9adb945e46da79a57d1
Jonne Kiukas <jonne.kiukas@hotmail.com>
2020-03-21 12:51:18 +0200

    Rate interface service and service are done.

--- RateTask/RateTask/Services/IRateService.cs
+++ RateTask/RateTask/Services/IRateService.cs
@@ -2,10 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using RateTask.Models; // Needs to be added.
 
 namespace RateTask.Services
 {
-	interface IRateService
+	public interface IRateService // Remember to make the interface pupblic.
 	{
+		RATE Create(RATE rate);
+		List<RATE> Read();
+		RATE Read(string id);
+		RATE Update(RATE rate);
+		void Delete(string id);
 	}
 }
--- RateTask/RateTask/Services/RateService.cs
+++ RateTask/RateTask/Services/RateService.cs
@@ -1,11 +1,52 @@
-﻿using System;
+﻿using RateTask.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using RateTask.Repositories; // Needs to be added.
 
 namespace RateTask.Services
 {
-	public class RateService
+	public class RateService : IRateService
 	{
+		// Inject repository layer
+		private readonly IRateRepository _rateRepository;
+
+		// Define constructor
+		public RateService(IRateRepository rateRepository)
+		{
+			_rateRepository = rateRepository;
+		}
+
+		// Create a new rate.
+		public RATE Create(RATE rate)
+		{
+			return _rateRepository.Create(rate);
+		}
+
+		// Delete a specific rate by id.
+		public void Delete(string id)
+		{
+			RATE deleteRate = _rateRepository.Read(id);
+			_rateRepository.Delete(deleteRate);
+		}
+
+		// Get all the rates in the list.
+		public List<RATE> Read()
+		{
+			return _rateRepository.Read();
+		}
+
+		// Get a specific customers by ID.
+		public RATE Read(string id)
+		{
+			return _rateRepository.Read(id);
+		}
+
+		// Update a specific rate by id.
+		public RATE Update(RATE rate)
+		{
+			return _rateRepository.Update(rate);
+		}
 	}
 }
